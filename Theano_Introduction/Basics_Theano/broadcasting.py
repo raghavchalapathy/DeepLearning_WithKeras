fa1a2095f78c2b6b0dc9e02c30b8389b9e2cb259
raghav <raghav.chalapathy@gmail.com>
2016-08-18 11:44:01 +1000

    example showing the broadcasting feature of theano

--- Theano_Introduction/Basics_Theano/broadcasting.py
+++ Theano_Introduction/Basics_Theano/broadcasting.py
@@ -4,11 +4,13 @@ import numpy as np
 import theano
 from theano import tensor as T
 
-# xval = np.array([[1, 2, 3], [4, 5, 6]])
-# bval = np.array([[10, 20, 30]])
-# print xval.shape
-# print bval.shape
-# print xval + bval
+
+# Numpy example of broadcasting
+xval = np.array([[1, 2, 3], [4, 5, 6]])
+bval = np.array([[10, 20, 30]])
+print xval.shape
+print bval.shape
+print xval + bval
 
 
 xval = np.array([[0.1], [0.2]])
@@ -17,15 +19,15 @@ print xval.shape
 print bval.shape
 print xval + bval
 
+# Theano equivalent declaring the variables as broadcastable
+x = T.dmatrix('x')
+b = theano.shared(bval, broadcastable=(True,False))
+z = x + b
+f = theano.function([x], z)
 
-# x = T.dmatrix('x')
-# b = theano.shared(bval, broadcastable=(True,False))
-# z = x + b
-# f = theano.function([x], z)
-#
 # print f(xval)
 
-
+# Another example of broadcastable feature of theano
 r = T.row('r')
 # print(r.broadcastable)
 
